338449da4605754a8a18f7df7057d1d2aac04845
anonymous9788 <bhuy652@gmail.com>
2024-04-10 17:50:56 +0700

    test3

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+var open = window_open();
+
 function directblitly_1() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Direct-blitly 1",
 		"content": "Hoàn thành nhiệm vụ Direct-blitly lần 1\nGửi ảnh ngay cho ad để được cộng 120🌾",
@@ -7,7 +9,6 @@ function directblitly_1() {
 		"encrypted": false,
 		"expire_at": null
 	};
-	var open = window.open()
 	fetch('https://anonm.my.eu.org/api/v2/paste', {
 		method: "POST",
 		headers: {
@@ -20,13 +21,13 @@ function directblitly_1() {
 		.then(res => res.json())
 		.then(data => {
 			// setTimeout(() => {
-				open.location= `https://direct.blitly.io/st?apikey=0442c51ef5b242518f997712ab75cfd4&url=https://anonm.my.eu.org/${data.paste.id}`;
+				open.location = `https://direct.blitly.io/st?apikey=0442c51ef5b242518f997712ab75cfd4&url=https://anonm.my.eu.org/${data.paste.id}`;
 			// });
 		});
 	return false;
 }
 async function directblitly_2() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Direct-blitly 2",
 		"content": "Hoàn thành nhiệm vụ Direct-blitly lần 2\nGửi ảnh ngay cho ad để được cộng 120🌾",
@@ -45,14 +46,14 @@ async function directblitly_2() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://direct.blitly.io/st?apikey=0442c51ef5b242518f997712ab75cfd4&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://direct.blitly.io/st?apikey=0442c51ef5b242518f997712ab75cfd4&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function blitly_1() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Blitly 1",
 		"content": "Hoàn thành nhiệm vụ Blitly lần 1\nGửi ảnh ngay cho ad để được cộng 200🌾",
@@ -71,14 +72,14 @@ async function blitly_1() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://blitly.io/st?apikey=408a8f82bd8c4438b2470139c9ec62d3&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://blitly.io/st?apikey=408a8f82bd8c4438b2470139c9ec62d3&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function blitly_2() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Blitly 2",
 		"content": "Hoàn thành nhiệm vụ Blitly lần 2\nGửi ảnh ngay cho ad để được cộng 200🌾",
@@ -97,14 +98,14 @@ async function blitly_2() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://blitly.io/st?apikey=408a8f82bd8c4438b2470139c9ec62d3&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://blitly.io/st?apikey=408a8f82bd8c4438b2470139c9ec62d3&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function web1s_1() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Web1s 1",
 		"content": "Hoàn thành nhiệm vụ Web1s lần 1\nGửi ảnh ngay cho ad để được cộng 100🌾",
@@ -123,14 +124,14 @@ async function web1s_1() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://web1s.com/st?token=e2a9c028-b032-437a-8a2f-129da76cd349&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://web1s.com/st?token=e2a9c028-b032-437a-8a2f-129da76cd349&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function web1s_2() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Web1s 2",
 		"content": "Hoàn thành nhiệm vụ Web1s lần 2\nGửi ảnh ngay cho ad để được cộng 100🌾",
@@ -149,14 +150,14 @@ async function web1s_2() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://web1s.com/st?token=e2a9c028-b032-437a-8a2f-129da76cd349&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://web1s.com/st?token=e2a9c028-b032-437a-8a2f-129da76cd349&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function web1s_3() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Web1s 3",
 		"content": "Hoàn thành nhiệm vụ Web1s lần 3\nGửi ảnh ngay cho ad để được cộng 100🌾",
@@ -175,14 +176,14 @@ async function web1s_3() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://web1s.com/st?token=fc7f2e14-c0a8-42cd-9036-04c3a32cd3cc&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://web1s.com/st?token=fc7f2e14-c0a8-42cd-9036-04c3a32cd3cc&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function web1s_4() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Web1s 4",
 		"content": "Hoàn thành nhiệm vụ Web1s lần 4\nGửi ảnh ngay cho ad để được cộng 100🌾",
@@ -201,14 +202,14 @@ async function web1s_4() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://web1s.com/st?token=fc7f2e14-c0a8-42cd-9036-04c3a32cd3cc&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://web1s.com/st?token=fc7f2e14-c0a8-42cd-9036-04c3a32cd3cc&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function web1s_5() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Web1s 5",
 		"content": "Hoàn thành nhiệm vụ Web1s lần 5\nGửi ảnh ngay cho ad để được cộng 100🌾",
@@ -227,14 +228,14 @@ async function web1s_5() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://web1s.com/st?token=fc7f2e14-c0a8-42cd-9036-04c3a32cd3cc&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://web1s.com/st?token=fc7f2e14-c0a8-42cd-9036-04c3a32cd3cc&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function mneylink() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Mneylink",
 		"content": "Hoàn thành nhiệm vụ Mneylink\nGửi ảnh ngay cho ad để được cộng 200🌾",
@@ -253,14 +254,14 @@ async function mneylink() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://mneylink.vip/st?api=26672f47309ffeb0fd1519c09f0affb067e31662&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://mneylink.vip/st?api=26672f47309ffeb0fd1519c09f0affb067e31662&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function mneydrec() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Mneydirec",
 		"content": "Hoàn thành nhiệm vụ Mneydirec\nGửi ảnh ngay cho ad để được cộng 150🌾",
@@ -279,14 +280,14 @@ async function mneydrec() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://mneydirec.com/st?api=b11f8df7f33bf77811f0fd939fb4f232164fa741&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://mneydirec.com/st?api=b11f8df7f33bf77811f0fd939fb4f232164fa741&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function normal_1() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Normal 1",
 		"content": "Hoàn thành nhiệm vụ Normal lần 1\nGửi ảnh ngay cho ad để được cộng 30🌾",
@@ -305,14 +306,14 @@ async function normal_1() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://web1s.com/st?token=cd7d7d71-620e-4c43-a241-000d9271e4ab&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://web1s.com/st?token=cd7d7d71-620e-4c43-a241-000d9271e4ab&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function normal_2() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Normal 2",
 		"content": "Hoàn thành nhiệm vụ Normal lần 2\nGửi ảnh ngay cho ad để được cộng 30🌾",
@@ -331,14 +332,14 @@ async function normal_2() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://web1s.com/st?token=cd7d7d71-620e-4c43-a241-000d9271e4ab&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://web1s.com/st?token=cd7d7d71-620e-4c43-a241-000d9271e4ab&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function memelink_1() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Memelink 1",
 		"content": "Hoàn thành nhiệm vụ Memelink lần 1\nGửi ảnh ngay cho ad để được cộng 120🌾",
@@ -357,14 +358,14 @@ async function memelink_1() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://memelink.net/st?apikey=ab47a395005e48e0b43fed1076063bf8&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://memelink.net/st?apikey=ab47a395005e48e0b43fed1076063bf8&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function memelink_2() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Memelink 2",
 		"content": "Hoàn thành nhiệm vụ Memelink lần 2\nGửi ảnh ngay cho ad để được cộng 120🌾",
@@ -383,14 +384,14 @@ async function memelink_2() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://memelink.net/st?apikey=ab47a395005e48e0b43fed1076063bf8&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://memelink.net/st?apikey=ab47a395005e48e0b43fed1076063bf8&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function teckurl() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Teckurl",
 		"content": "Hoàn thành nhiệm vụ Teckurl\nGửi ảnh ngay cho ad để được cộng 40🌾",
@@ -409,16 +410,16 @@ async function teckurl() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://teckurl.click/apiv1.php?token=1NSnALke0lNmjwIuYFqwZli7ydNocVxn&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://teckurl.click/apiv1.php?token=1NSnALke0lNmjwIuYFqwZli7ydNocVxn&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function ggoklink() { }
 
 async function bb1() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Bitlybox 1",
 		"content": "Hoàn thành nhiệm vụ Bitlybox lần 1\nGửi ảnh ngay cho ad để được cộng 250🌾",
@@ -437,14 +438,14 @@ async function bb1() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://bitlybox.com/api/v1/short-link/ql?token=V16vUYZgxu147omJE275Z9SlpKI8ZCGi&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://bitlybox.com/api/v1/short-link/ql?token=V16vUYZgxu147omJE275Z9SlpKI8ZCGi&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function bb2() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Bitlybox 2",
 		"content": "Hoàn thành nhiệm vụ Bitlybox lần 2\nGửi ảnh ngay cho ad để được cộng 250🌾",
@@ -463,14 +464,14 @@ async function bb2() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://bitlybox.com/api/v1/short-link/ql?token=V16vUYZgxu147omJE275Z9SlpKI8ZCGi&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://bitlybox.com/api/v1/short-link/ql?token=V16vUYZgxu147omJE275Z9SlpKI8ZCGi&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function link1m1() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Link1m 1",
 		"content": "Hoàn thành nhiệm vụ Link1m lần 1\nGửi ảnh ngay cho ad để được cộng 300🌾",
@@ -489,14 +490,14 @@ async function link1m1() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://link1m.com/st?api=20014dc205acb4e0a954dc74634835d36458d560&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://link1m.com/st?api=20014dc205acb4e0a954dc74634835d36458d560&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
 async function link1m2() {
-	var paste = {
+	let paste = {
 		"type": "PASTE",
 		"title": "Link1m 1",
 		"content": "Hoàn thành nhiệm vụ Link1m lần 2\nGửi ảnh ngay cho ad để được cộng 300🌾",
@@ -515,9 +516,9 @@ async function link1m2() {
 	})
 		.then(res => res.json())
 		.then(data => {
-			setTimeout(() => {
-				window.open('https://link1m.com/st?api=20014dc205acb4e0a954dc74634835d36458d560&url=https://anonm.my.eu.org/' + String(data.paste.id), '_blank');
-			});
+			// setTimeout(() => {
+				open.location = `https://link1m.com/st?api=20014dc205acb4e0a954dc74634835d36458d560&url=https://anonm.my.eu.org/${data.paste.id}`;
+			// });
 		});
 	return false;
 }
